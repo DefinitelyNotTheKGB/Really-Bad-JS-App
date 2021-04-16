e65c5a444574812159baf55509f97189c7c8470d
BSteffaniakSS <74194823+BSteffaniakSS@users.noreply.github.com>
2021-04-16 15:30:12 -0400

    Add more injection examples

--- file.js
+++ file.js
@@ -8,3 +8,20 @@ $(".my-class").html(x);
 var sneakyString = document.location.href.substring(document.location.href.indexOf("content=") + 8);
 
 $(".my-class").html(sneakyString);
+
+var valueToInject = randomStringFromSomeRandomPlace;
+
+$(".my-class").html(valueToInject);
+
+$.post("my post request url thing", (response) => {
+    $(".my-class").html(response.message);
+});
+
+// .text() should be fine
+$(".my-class").text(sneakyString);
+
+$(".my-class").text(valueToInject);
+
+$.post("my post request url thing", (response) => {
+    $(".my-class").text(response.message);
+});

03966e5f109e6c0e76fb666a1246ee8d70c5b676
BSteffaniakSS <74194823+BSteffaniakSS@users.noreply.github.com>
2021-04-16 15:19:46 -0400

    Add $.html stuff

--- file.js
+++ file.js
@@ -1,1 +1,10 @@
 eval(document.location.href.substring(document.location.href.indexOf("default=")+8));
+
+var x = "hey";
+
+// literal string should be fine
+$(".my-class").html(x);
+
+var sneakyString = document.location.href.substring(document.location.href.indexOf("content=") + 8);
+
+$(".my-class").html(sneakyString);
